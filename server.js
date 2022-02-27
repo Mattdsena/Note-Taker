fe5b84e4cf9d34ccdf960678d90216f2f0649983
Matt DSena <matthewdsena@gmail.com>
2022-02-27 17:29:52 +1030

    brain is not working, moved files to correct locations

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-const express = require('express');
-
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const app = require('express');
 
 const app = express();
-
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.json());
-
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`API server is ready on port ${PORT}!`);
